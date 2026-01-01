643cbf0ec5e099dff60e7163efdebd98760a1485
EasterEggser2 <axelislas123a@gmail.com>
2026-01-01 20:24:33 -0300

    Update index.tsx

--- routes/index.tsx
+++ routes/index.tsx
@@ -7,20 +7,15 @@ import { Head } from "fresh/runtime";
 export default function Inicio() {
   return (
     <>
-      <Head>
-        {/* El preload está perfecto, mantenlo así */}
-        <link rel="preload" href="/img/Hero.webp" as="image" fetchpriority="high" />
-      </Head>
-      
       <main className="bg-Azul text-Blanco overflow-x-hidden flex flex-col">
         <div className="relative w-full h-screen bg-Azul shrink-0">
           <img
             src="/img/Hero.webp"
-            fetchpriority="high" 
+            fetchpriority="high"
             loading="eager"
-            decoding="sync"
+            decoding="sync" 
             className="absolute inset-0 w-full h-full object-cover object-[50%_10%]"
-            alt="Hero Btoq Challengers"
+            alt="Hero Btoq"
           />
           <div className="relative flex flex-col items-center justify-center h-full">
           </div>
